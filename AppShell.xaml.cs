899639281e39ecae0a76e392f549a64d813caab1
HappyElectron <ned.fpartridge@gmail.com>
2024-08-14 20:36:51 +1000

    Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SpraywallAppMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SpraywallAppMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
