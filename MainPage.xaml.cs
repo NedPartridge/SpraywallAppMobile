8eca4541d67b0598a4b02fc94bf5ad66d10af8ea
HappyElectron <ned.fpartridge@gmail.com>
2024-08-15 15:40:18 +1000

    Button + bg
    Oh dear this is going to be a late night

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,16 @@
-﻿namespace SpraywallAppMobile
+﻿using System.Diagnostics;
+
+namespace SpraywallAppMobile
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void LoginButtonClicked(object sender, TappedEventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Console.WriteLine("Login clicked!");
         }
     }
 
