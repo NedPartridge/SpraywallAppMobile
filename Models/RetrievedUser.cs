fccb5ae4557ee476ecd6b4b0496af0505fa734dc
HappyElectron <ned.fpartridge@gmail.com>
2024-08-31 22:55:07 +1000

    this shouldve been included

---
+++ Models/RetrievedUser.cs
@@ -0,0 +1,11 @@
+﻿namespace SpraywallAppMobile.Models;
+
+// Class representing a 'user' object, retrieved from the server
+// 
+// Used when initialising user settings, not to be sent back
+class RetrievedUser
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
